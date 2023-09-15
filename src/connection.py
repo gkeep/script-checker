77438e27c443447c0813bd28d64c7edf0deaae7f
Gennady Koshkin <me@gkeep.dev>
2023-09-15 10:39:47 +0300

    Add connection test and wrap everything in a class

--- src/connection.py
+++ src/connection.py
@@ -3,27 +3,37 @@ import paramiko
 from config import Config
 
 
-def get_connection():
-    cfg = Config()
+class SSHClient:
+    client = None
 
-    user = cfg.machine['username']
-    host = cfg.machine['address']
-    port = cfg.machine['port']
+    def __init__(self):
+        self.get_connection()
 
-    print(f"Connecting to {user}@{host}:{port}...")
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(
-        hostname=host,
-        port=port,
-        username=user,
-        key_filename=cfg.ssh_key['path'],
-        passphrase=cfg.ssh_key['password']
-    )
+    def get_connection(self):
+        cfg = Config()
 
-    stdin, stdout, stderr = ssh.exec_command('uname -a')
-    print(stdout.read())
+        user = cfg.machine['username']
+        host = cfg.machine['address']
+        port = cfg.machine['port']
+
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(
+            hostname=host,
+            port=port,
+            username=user,
+            key_filename=cfg.ssh_key['path'],
+            passphrase=cfg.ssh_key['password']
+        )
+
+        self.client = ssh
+
+    def test_connection(self):
+        stdin, stdout, stderr = self.client.exec_command('cat /etc/os-release')
+        return stdout.readline().strip()
 
 
-if __name__ == "__main__":
-    get_connection()
+def test_connection():
+    ssh = SSHClient()
+    out = ssh.test_connection()
+    assert out == 'NAME="Ubuntu"'

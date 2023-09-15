0610c656ded7a5ba485c8135295ac048c65e2f1f
Gennady Koshkin <me@gkeep.dev>
2023-09-15 11:52:21 +0300

    Upload script to remote machine and run checks on it

--- src/connection.py
+++ src/connection.py
@@ -1,4 +1,6 @@
 import paramiko
+from scp import SCPClient
+import os.path
 
 from config import Config
 
@@ -32,8 +34,48 @@ class SSHClient:
         stdin, stdout, stderr = self.client.exec_command('cat /etc/os-release')
         return stdout.readline().strip()
 
+    def put_file(self, file_path: str):
+        scp = SCPClient(self.client.get_transport())
+
+        abs_path = os.path.expanduser(file_path)
+        scp.put(abs_path, remote_path="/tmp")
+
+    def run_file(self, file_path: str):
+        out = []
+
+        def run_command(command: str):
+            stdin, stdout, stderr = self.client.exec_command(command)
+            _stdout = stdout.readlines()
+            _stderr = stderr.readlines()
+            return {
+                "command": command,
+                "stdout": _stdout,
+                "stderr": _stderr
+            }
+
+        file_name = file_path.split("/")[-1]
+        remote_file = "/tmp/" + file_name
+
+        out.append(run_command(f"chmod +x {remote_file}"))
+        out.append(run_command(f"shellcheck {remote_file} --exclude SC2086"))
+        for item in out:
+            if item['stderr'] is None and item['stdout'] is None:
+                out.append(run_command(f"bash -x {remote_file}"))
+
 
 def test_connection():
     ssh = SSHClient()
     out = ssh.test_connection()
     assert out == 'NAME="Ubuntu"'
+
+
+def test_file_upload():
+    file = "~/Desktop/scripts/ibs-undelete.sh"
+    ssh = SSHClient()
+    ssh.put_file(file_path=file)
+
+
+def test_run_file():
+    file = "~/Desktop/scripts/ibs-undelete.sh"
+    ssh = SSHClient()
+    ssh.run_file(file_path=file)
